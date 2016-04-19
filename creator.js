03eb5b8c165fcad2d10303fb8d8f7f1a4f32d58e
Tyler Bonnell <tbonnell@uw.edu>
2016-04-19 15:03:48 -0700

    animation

--- creator.js
+++ creator.js
@@ -2,17 +2,21 @@
 var anim = {
   "frames": [[["", "black", ""],
               ["black", "black", "black"],
-              ["", "black", ""]]]
+              ["", "black", ""]],
+             [["black", "", "black"],
+              ["", "black", ""],
+              ["black", "", "black"]]]
 };
 
 
 var canvas;
 window.onload = function() {
-  canvas = new PixelCanvas(40, 40, 5, $("body"), 100000, "cornflowerblue", function(px) {
+  canvas = new PixelCanvas(40, 40, 5, $("body"), 1000, "cornflowerblue", function(px) {
     px.onclick = function() {
       this.style.backgroundColor = "red";
     };
   });
+
   canvas.add(anim);
   canvas.start();
 };
